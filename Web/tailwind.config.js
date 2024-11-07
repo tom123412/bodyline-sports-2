96e0cd1f859dfcee98c0401b31ef871e22d6528a
tom p <txp001@hotmail.com>
2024-11-07 11:58:20 +1100

    added tailwind support

---
+++ Web/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Components/**/*.razor"],
+  safelist: [
+  ],
+  theme: {
+    extend: {
+      colors: {
+        facebook: '#0866FF',
+      },
+    },
+  },
+  plugins: [
+  ],
+}
+
