f2fbe2e47cc831126b803e890127aaea21ba6f41
tom123412 <80753203+tom123412@users.noreply.github.com>
2025-06-01 09:56:03 +0000

    use current domain for redirect uri

--- Web/Facebook/FacebookPost.cs
+++ Web/Facebook/FacebookPost.cs
@@ -1,6 +1,13 @@
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 
 namespace Web.Facebook;
 
-public record FacebookPost(string Id, string? Message, DateTimeOffset UpdatedDateTime, 
-    string? Type, FacebookAttachment[] Attachments);
+public partial record FacebookPost(string Id, string? Message, DateTimeOffset UpdatedDateTime,
+    string? Type, FacebookAttachment[] Attachments)
+{
+    public string[] Reels => [.. MyRegex().Matches(Message ?? string.Empty).Select(m => m.Groups[1].Value)];
+
+    [GeneratedRegex(@"(https://www.facebook.com/reel/[0-9]+)")]
+    private static partial Regex MyRegex();
+}
