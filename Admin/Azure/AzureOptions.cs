c711d7e4d2f02d855a4a27591b32c557d0a8562b
tom123412 <80753203+tom123412@users.noreply.github.com>
2025-06-02 09:39:08 +0000

    use connection string if provided when updating app config

--- Admin/Azure/AzureOptions.cs
+++ Admin/Azure/AzureOptions.cs
@@ -3,4 +3,5 @@ namespace Admin.Azure;
 public class AzureOptions
 {
     public required Uri AppConfigurationEndpoint { get; set; }
+    public string? AppConfigurationConnectionString { get; set; }
 };
