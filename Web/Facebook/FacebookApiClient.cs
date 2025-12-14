d6c492359443fe99671b1869f489561382c87273
tom123412 <80753203+tom123412@users.noreply.github.com>
2025-12-14 07:04:33 +0000

    added method to call new sse endpoint to get posts

--- Web/Facebook/FacebookApiClient.cs
+++ Web/Facebook/FacebookApiClient.cs
@@ -1,34 +1,37 @@
 using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 
 namespace Web.Facebook;
 
-public class FacebookApiClient(HttpClient httpClient, IOptions<FacebookOptions> options, IMemoryCache cache)
+public class FacebookApiClient(HttpClient httpClient, IOptions<FacebookOptions> options)
 {
     private readonly FacebookOptions _options = options.Value;
 
     public async Task<FacebookGroup?> GetGroup(CancellationToken cancellationToken = default)
     {
-        var x = httpClient;
-        var group = await httpClient.GetFromJsonAsync<FacebookGroup>($"/api/facebook/groups/{_options.GroupId}", cancellationToken: cancellationToken);
+        var group = await httpClient.GetFromJsonAsync<FacebookGroup>($"/api/facebook/groups/{_options.GroupId}", cancellationToken);
         return group;
     }
 
     public async Task<IEnumerable<FacebookPost>> GetPostsForGroup(CancellationToken cancellationToken = default)
     {
-        const string PostsCacheKey = "FacebookPosts";
-        var CacheDuration = TimeSpan.FromMinutes(5);
+        var posts = await httpClient.GetFromJsonAsync<IEnumerable<FacebookPost>>(
+            $"/api/facebook/groups/{_options.GroupId}/posts", cancellationToken);
 
-        return (await cache.GetOrCreateAsync(PostsCacheKey, async entry =>
-        {
-            entry.AbsoluteExpirationRelativeToNow = CacheDuration;
+        return posts ?? [];
+    }
 
-            var posts = await httpClient.GetFromJsonAsync<IEnumerable<FacebookPost>>(
-                $"/api/facebook/groups/{_options.GroupId}/posts",
-                cancellationToken: cancellationToken);
+    public async Task<IAsyncEnumerable<FacebookPost>> GetPostsForGroup2(CancellationToken cancellationToken = default)
+    {
+        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, $"/api/facebook/groups/{_options.GroupId}/posts");
+        //httpRequestMessage.SetBrowserResponseStreamingEnabled(true);
+        //httpRequestMessage.Headers.Add("Accept", "text/event-stream");
 
-            return posts ?? [];
-        })) ?? [];
+        var response = await httpClient.SendAsync(httpRequestMessage, cancellationToken);
+        var posts = await response.Content.ReadFromJsonAsync<IAsyncEnumerable<FacebookPost>>(cancellationToken);
+
+        return posts ?? AsyncEnumerable.Empty<FacebookPost>();
     }
 }
